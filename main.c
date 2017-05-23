c47d37be9470e07972be86871bfc2c6cd91dea02
sanchoyz <sania.pey4ev@yandex.ru>
2017-05-23 09:25:52 +0700

    add main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+float x,eps,k,f=0,a, r=1;
+printf ("Vvedite x: ");
+scanf ("%f",&x);
+printf ("Vvedite eps: ");
+scanf ("%f",&eps);
+k=0;
+a = x;
+f = a;
+while (!((a<eps)&&(-eps<a)))
+{
+k=k+1;
+r = -2*x/(2*k+1);
+a=a*r;
+f+=a;
+printf ("\n%f", a);
+}
+printf ("\n%f", f);
+return 0;
+}
